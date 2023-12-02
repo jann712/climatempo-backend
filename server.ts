6627b78d26a989d05f1d6a8ebf91d842c3d25ebe
jann712 <joao.augustonn1@gmail.com>
2023-12-02 10:33:29 -0300

    data added

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 import "dotenv/config";
 
 const redisURL = process.env.REDIS_URL;
-const redisClient = createClient({ url: redisURL });
+const redisClient = createClient({ url: redisURL }).on('error', err => console.log('Redis Client Error', err))
 const app = express();
 const port = 3000;
 const DEFAULT_EXPIRATION = 3600;
@@ -29,22 +29,24 @@ app.get("/clima", async (req, res) => {
       const { data } = await axios.get(
         `http://apiadvisor.climatempo.com.br/api/v1/anl/synoptic/locale/BR?token=${token}`
       );
-      await redisClient.setEx("clima", 3600, JSON.stringify(data));
+      await redisClient.setEx("clima", 60, JSON.stringify(data));
       data[0]["execTime"] = performance.now() - execStart;
+      data[0]["cached"] = false
       console.log("clima is null");
       res.status(201).json(data);
     } else if (response != null) {
       console.log("clima is cached");
       const jsonData = JSON.parse(response);
       jsonData[0]["execTime"] = performance.now() - execStart;
+      jsonData[0]["cached"] = true
 
       res.status(201).json(jsonData);
     }
-    
+
     res.status(404).send();
   });
 });
 app.listen(port, async () => {
-  await redisClient.connect();
+  { !(redisClient.isReady) ? await redisClient.connect() : console.log("Redis connection already open.")}
   console.log(`Server started at port ${port}`);
 });
